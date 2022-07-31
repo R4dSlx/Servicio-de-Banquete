6253765e841ead5d1bcf2fdb7e2feccca1dd28aa
unknown <jloor2378@utm.edu.ec>
2022-07-31 15:21:35 -0500

    UPDATE 31

--- Proyecto_BankeT/settings.py
+++ Proyecto_BankeT/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-!7p8_v7fy#!!sw2uf7ctiaoh--vcd5#crt_&e5j#yd#@kmoge_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [enigmatic-lowlands-40782.herokuapp.com]
+ALLOWED_HOSTS = []
 
 
 # Application definition
